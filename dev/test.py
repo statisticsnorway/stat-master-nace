5e81eb145e7b2552d6638b24d12ce34efab57943
Minauio <minash19@yahoo.com>
2025-07-17 12:43:16 +0200

    added a dev folder

---
+++ dev/test.py
@@ -0,0 +1,15 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#   kernelspec:
+#     display_name: Python3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# hello world
