c78a2a79d5759c4d82f3021ed21797a8ca6d4db5
Mina <minash19@yahoo.com>
2025-11-11 01:41:28 +0200

    fixed and ran the hierarchical model

---
+++ run_hier_explore.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=thesis
+#SBATCH --account=project_465002259
+#SBATCH --partition=dev-g
+#SBATCH --time=3:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=7
+#SBATCH --mem=32G
+#SBATCH --gpus-per-node=6
+
+
+# NB: this script should be run with "sbatch run_classifier.slurm"!
+# See https://www.uio.no/english/services/it/research/platforms/edu-research/help/fox/jobs/submitting.md
+
+source ~/.bashrc
+
+# sanity: exit on all errors and disallow unset environment variables
+set -o errexit
+set -o nounset
+
+# the important bit: unload all current modules (just in case) and load only the necessary ones
+module purge
+export EBU_USER_PREFIX=/projappl/project_465002259/software/
+module load LUMI
+module load PyTorch/2.6.0-rocm-6.2.4-python-3.12-singularity-20250404
+
+export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_PORT=9999
+export WORLD_SIZE=$SLURM_NTASKS
+export RANK=$SLURM_PROCID
+export LOCAL_RANK=$SLURM_LOCALID
+
+
+# by default, pass on any remaining command-line options
+srun --label python3 run_hier_explore.py $@
