d2fd08246ff83b33b78d488de1201b6e50529562
Mina <minashe@uio.no>
2026-01-25 18:04:51 +0100

    fixed bugs in llm

--- llm_classifier_hier.py
+++ llm_classifier_hier.py
@@ -46,7 +46,7 @@ def build_prompt(descriptions, level_name, options, parent=None):
     return prompts
 
 
-def extract_class(output_text):
+def extract_class(output_text, hierarchy, level, map_code_names):
     # Removeing "assistant" prefix if present
     assistant_prefix = "assistant\n\n"
     if output_text.startswith(assistant_prefix):
@@ -55,10 +55,18 @@ def extract_class(output_text):
     if output_text is None:
         print(f"No code found in model output:\n{output_text}")
         return None
+    
+    if output_text not in hierarchy[level]:
+        if output_text in map_code_names:
+            output_text = map_code_names[output_text]
+        else:
+            print(f"{output_text} not in HIERARCHY")
+            return None
+        
     return output_text
 
 
-def llm_call(tokenizer, llm, sampling_params, prompts):
+def llm_call(tokenizer, llm, sampling_params, prompts, hierarchy, level, map_code_names):
     """
     texts: list of strings
     returns: list of (text, score)
@@ -87,10 +95,10 @@ def llm_call(tokenizer, llm, sampling_params, prompts):
         probs = out.outputs[0].logprobs
         print('--------- raw prompt ------------')
         print(raw)
-        nace_code = extract_class(raw)
+        nace_code = extract_class(output_text=raw, hierarchy=hierarchy, level=level, map_code_names=map_code_names)
         results.append(nace_code)
         results_probs.append(probs)
-        print(results_probs) #----------------- legge til sannsynlighetene i run functionen
+        print('############### result logits \n', results_probs) #----------------- legge til sannsynlighetene i run functionen
     return results
 
 
@@ -107,37 +115,12 @@ def format_output(id, text, score, gold_score=None):
     print("----------- OUT PROMPT-----------")
     print(data_res)
     return data_res
-
-
-
-SECTIONS = [
-    "A – Agriculture, forestry and fishing",
-    "B – Mining and quarrying",
-    "C – Manufacturing",
-    "D – Electricity, gas, steam and air conditioning supply",
-]
 """
 
 
 def sections_df_hier(df:pd.DataFrame):
     sections = df[df['level']==1][['code','name']].astype(str).agg(" - ".join, axis=1).tolist()
     return sections
-
-
-"""
-HIERARCHY = {
-    # Section → Division
-    "C": ["10", "11", "12"],
-
-    # Division → Group
-    "10": ["101", "102"],
-
-    # Group → Class
-    "101": ["1011", "1012"],
-
-    # Class → Subclass
-    "1011": ["10110", "10120"],
-}"""
 
 
 def derive_hier_names(df: pd.DataFrame, subclass_col:str, section_map, map_name):
@@ -147,6 +130,10 @@ def derive_hier_names(df: pd.DataFrame, subclass_col:str, section_map, map_name)
     names_div = codes_div.map(map_name)
     df["division"] = codes_div + " - " + names_div
 
+    codes_sec = codes_div.map(section_map) 
+    names_sec = codes_sec.map(map_name)
+    df["section"] = codes_sec + " - " + names_sec
+
     codes_gr = df[subclass_col].str[:4]
     names_gr = codes_gr.map(map_name)
     df["group"] = codes_gr + " - " + names_gr
@@ -155,9 +142,34 @@ def derive_hier_names(df: pd.DataFrame, subclass_col:str, section_map, map_name)
     names_cls = df[subclass_col].map(map_name)
     df["class"] = codes_cls + " - " + names_cls
 
-    df["section"] = df["division"].map(section_map)
     return df
 
+def mapping_code_names(df: pd.DataFrame, subclass_col:str, section_map:dict, map_name:dict):
+    """  ["section", "division", "group", "class"] """
+    df = df.copy()
+
+    df_div = pd.DataFrame({
+    "code": df[subclass_col].str[:2],
+    "name": df[subclass_col].str[:2] + " - " + df[subclass_col].str[:2].map(map_name)
+    })
+
+    df_sec = pd.DataFrame({
+        "code": df[subclass_col].str[:2].map(section_map),
+        "name": df[subclass_col].str[:2].map(section_map) + " - " + df[subclass_col].str[:2].map(section_map).map(map_name)
+    })
+
+    df_gr = pd.DataFrame({
+        "code": df[subclass_col].str[:4],
+        "name": df[subclass_col].str[:4] + " - " + df[subclass_col].str[:4].map(map_name)
+    })
+
+    df_cls = pd.DataFrame({
+        "code": df[subclass_col].str[:5],
+        "name": df[subclass_col].str[:5] + " - " + df[subclass_col].map(map_name)
+    })
+
+    map_df = pd.concat([df_div, df_sec, df_gr, df_cls], ignore_index=True).drop_duplicates().set_index("code")["name"].to_dict()
+    return map_df
 
 
 def build_parent_child_map(df, hier=['section', 'division', 'group', 'class', 'code']):
@@ -174,16 +186,24 @@ def build_parent_child_map(df, hier=['section', 'division', 'group', 'class', 'c
 
     return all_parent_child
 
-
-
 def auto_descend(code, hierarchy):
     """
     Walk down hierarchy automatically
     while only ONE child exists.
     """
-    while code in hierarchy and len(hierarchy[code]) == 1:
-        code = hierarchy[code][0]
-    return code
+    child_level = {
+    "section":"division",
+    "division": "group",
+    "group": "class",
+    "class" :"subclass"
+    }
+    for level in ["section", "division", "group", "class"]:
+        if code in hierarchy[level] and len(hierarchy[level][code]) == 1:
+            code = hierarchy[level][code][0]
+        else:
+            return code, level
+    
+    return code, child_level[level]
 
 
 
@@ -191,19 +211,30 @@ def run_classify_nace(tokenizer,
                       llm,
                       sections,
                       hierarchy,
+                      map_code_names,
                       input_file:str=args.val_data_file, 
                       input:str|list[str] = ["company_activity", "company_name"],
                       sampling_params=sampling_params, 
                       batch_size=args.batch_size, 
                       output_file:str=args.output_file,
                       ):
-    results = {}
+    #results = {}
+    levels = ["section", "division", "group", "class", "subclass"]
+    map_hier_indx = {level: i for i, level in enumerate(levels)}
+
+    parent_level = {
+        "division": "section",
+        "group": "division",
+        "class": "group",
+        "subclass": "class"
+    }
+
     first_batch = True
 
     for batch in pd.read_csv(input_file, chunksize=batch_size):
 
         descriptions=batch[input].fillna("").astype(str).agg(" ".join, axis=1).tolist()
-        
+        print('################## descriptions \n', descriptions)
         # results per company
         batch_results = {i: {} for i in range(len(descriptions))}
 
@@ -215,72 +246,95 @@ def run_classify_nace(tokenizer,
             options=sections
         )
 
+        print('################## section prompt \n', section_prompt)
+
         section_preds = llm_call(
             tokenizer=tokenizer, 
             llm=llm, 
             sampling_params=sampling_params,
-            prompts=section_prompt)# e.g. "C"
+            prompts=section_prompt,
+            hierarchy=hierarchy, 
+            level="section", 
+            map_code_names=map_code_names)# e.g. "C"
         
-        ### ----------- might need extraction --------------
+        print('################ section peds \n', section_preds)
 
+
+        prompts = []
+        options_list = []
+
+        # auto-descend to fill unambiguous levels
         for i, pred in enumerate(section_preds):
-            final_code = auto_descend(pred, hierarchy)
-            batch_results[i]["section"] = final_code
+            final_code, final_level = auto_descend(pred, hierarchy)
+            batch_results[i][final_level] = final_code
 
-        results["section"] = section_preds
-        current = section_preds
+        # building prompts for next predictions
+        for i, res in batch_results.items():
+            # Skip if already at deepest level
+            last_known_level = max(res.keys(), key=lambda k: map_hier_indx[k]) if res else None
+            
+            if last_known_level == "subclass":
+                continue  # nothing left to predict
 
-        # Division until Subclass
-        for level in ["division", "group", "class", "subclass"]:
-            # Prepare prompts only for companies that still need prediction
-            indices_to_predict = [i for i, res in batch_results.items() if level not in res]
+            # Determine next level to predict
+            if last_known_level is None:
+                next_level = "section"
+                parent = None
+                options = list(hierarchy[next_level].keys())
+            else:
+                last_parent = res[last_known_level]
+                next_level_index = map_hier_indx[last_known_level] + 1
+                next_level = levels[next_level_index]
 
-            if not indices_to_predict:
-                continue
-
-            prompts = []
-            options_list = []
-
-            for i in indices_to_predict:
-                parent = batch_results[i].get({
-                    "division": "section",
-                    "group": "division",
-                    "class": "group",
-                    "subclass": "class"
-                }[level])
-
-                if parent not in hierarchy or len(hierarchy[parent]) == 0:
-                    batch_results[i][level] = parent
-                    print(f"{current} not in HIERARCHY")
+                # Check that parent exists in hierarchy
+                if last_parent not in hierarchy.get(last_known_level, {}):
+                    print(f"{last_parent} not in hierarchy for level {last_known_level}")
                     continue
-                
-                prompts.append(build_prompt(
-                    descriptions=descriptions,
-                    level_name=level,
-                    options=hierarchy[parent],
-                    parent=current
-                ))
-                options_list.append(hierarchy[current])
 
-            if not prompts:
-                continue
+                options = hierarchy[last_known_level][last_parent]
+                if not options:
+                    continue  # nothing to predict
+
+            # auto-descend if one child
+            if len(options) == 1:
+                auto_code, auto_level = auto_descend(options[0], hierarchy)
+                batch_results[i][auto_level] = auto_code
+                continue  # no prompt needed
+
+            # build prompt for the next level
+            buildt_prompt = build_prompt(
+                descriptions=descriptions,
+                level_name=next_level,
+                options=options,
+                parent=last_parent
+            )
+            prompts.extend(buildt_prompt)
+            options_list.append(options)
 
             preds = llm_call(tokenizer=tokenizer, 
-                                    llm=llm, 
-                                    sampling_params=sampling_params,
-                                    prompts=prompts)
+                             llm=llm, 
+                             sampling_params=sampling_params,
+                             prompts=prompts,
+                             hierarchy=hierarchy, 
+                             level=next_level, 
+                             map_code_names=map_code_names
+                             )
+            
 
-            # Save predictions with auto-descend
-            for j, i in enumerate(indices_to_predict):
-                pred = preds[j]
+            
 
-                # Validate prediction
-                if pred not in options_list[j]:
-                    print(f"Invalid prediction at {level}: {pred}, using parent {batch_results[i].get(level, '')}")
-                    pred = batch_results[i].get(level, '')
+            # Validate prediction
+            for pred, option in zip(preds, options_list):
+                if set(pred).issubset(option):
+                    print(f"Invalid prediction at {next_level}: {pred}, using parent {batch_results[i].get(next_level, '')}")
+                    pred = batch_results[i].get(next_level, '')
 
-                final_code = auto_descend(pred, hierarchy)
-                batch_results[i][level] = final_code
+            #if preds not in options_list:
+            #    print(f"Invalid prediction at {next_level}: {pred}, using parent {batch_results[i].get(next_level, '')}")
+            #    pred = batch_results[i].get(next_level, '')
+
+            final_code = auto_descend(pred, hierarchy)
+            batch_results[i][next_level] = final_code
 
         # Add predictions to batch DataFrame
         for i, res in batch_results.items():
@@ -308,12 +362,14 @@ if __name__ == "__main__":
     tokenizer = model.get_tokenizer()
    
     SECTIONS = sections_df_hier(df_hier)
-    map_sec = dict(zip(df_hier["code"], df_hier["parentCode"]))
+    map_sec = dict(zip(df_hier[df_hier['level']==2]["code"], df_hier[df_hier['level']==2]["parentCode"]))
     map_name = df_hier[['code', 'name']].set_index('code')['name'].to_dict()
-    df_hier_hier = derive_hier_names(df=df_hier,  subclass_col='code', section_map=map_sec, map_name=map_name)
+    df_hier_hier = derive_hier_names(df=df_hier[df_hier['level']==5],  subclass_col='code', section_map=map_sec, map_name=map_name)
     HIERARCHY = build_parent_child_map(df_hier_hier)
+    map_code_names = mapping_code_names(df=df_hier[df_hier['level']==5],  subclass_col='code', section_map=map_sec, map_name=map_name)
     
-    df_with_res = run_classify_nace(tokenizer=tokenizer, llm=model, sections=SECTIONS, hierarchy=HIERARCHY)
+
+    df_with_res = run_classify_nace(tokenizer=tokenizer, llm=model, sections=SECTIONS, hierarchy=HIERARCHY, map_code_names=map_code_names)
     print(df_with_res['subclass'])
     res_sub, res_cl, res_gro, res_div = metrics_levels(target=df_with_res['nace_21_code'], pred=df_with_res['subclass']) # type: ignore
     
